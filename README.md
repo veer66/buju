4eb0d1bb28c3c686cbd88475086eb6afc1febeb8
Vee Satayamas <vsatayamas@gmail.com>
2017-12-14 18:52:27 +0700

    Usable

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # buju
 A simple geosparql server
+
+
+## Build
+````
+mvn clean compile
+````
+
+## Example
+
+### Run server
+````
+mvn exec:java -Dexec.mainClass="rocks.veer66.AppKt"
+````
+
+### Query
+````
+curl -v -XPOST localhost:3012/query --data '@example/ex1.sparql'
+````
--- src/main/kotlin/rocks/veer66/app.kt
+++ src/main/kotlin/rocks/veer66/app.kt
@@ -5,11 +5,17 @@ import rocks.veer66.query.query
 import me.grison.jtoml.impl.Toml
 import java.io.File
 import io.ktor.application.*
+import io.ktor.content.readText
 import io.ktor.http.*
+import io.ktor.request.receiveText
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import org.apache.jena.query.ResultSetFormatter
+import java.io.ByteArrayOutputStream
+import java.io.StringWriter
+import java.io.Writer
 
 fun main(args: Array<String>) {
     println(System.getProperty("user.dir"))
@@ -19,16 +25,20 @@ fun main(args: Array<String>) {
     val idxDir = conf.get("idx_dir") as String
     val ttlPath = conf.get("ttl_path") as String
     val dataset = createDatasetFromTtl(ttlPath, idxDir)
-    query(dataset, """PREFIX spatial: <http://jena.apache.org/spatial#>
-        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-        SELECT ?p  WHERE {
-            ?p spatial:nearby (12.6813 101.2816 1000.0 'km') .
-        }""")
 
     val server = embeddedServer(Netty, port.toInt()) {
         routing {
             get("/") {
-                call.respondText("Hello, world!", ContentType.Text.Html)
+                call.respondText("Buju!", ContentType.Text.Html)
+            }
+            post("/query") {
+                call.respondText(ContentType.Application.Json) {
+                    val q = call.receiveText()
+                    val results = query(dataset, q)
+                    val buf = ByteArrayOutputStream()
+                    ResultSetFormatter.outputAsJSON(buf, results);
+                    String(buf.toByteArray())
+                }
             }
         }
     }
